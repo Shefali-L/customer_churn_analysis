6a5029890e7f6c45ab2b2c934f03ebc7ee2e4c84
Shefali-L <shefalilewis510@gmail.com>
2026-09-24 13:34:26 +0530

    Add files via upload

---
+++ CustomerChurnBasicQuery.sql
@@ -0,0 +1,33 @@
+select Gender, count(Gender) as TotalCount,
+count(Gender) * 100.0/ (select count(*) from stg_churn) as Percentage
+from stg_churn
+group by Gender
+
+
+
+select Contract, count(Contract) as TotalCount,
+count(Contract) * 1.0/ (select count(*) from stg_churn) as Percentage
+from stg_churn
+group by Contract
+
+
+
+
+select Customer_Status, count(Customer_Status) as TotalCount, sum(Total_Revenue) as TotalRev,
+sum(Total_Revenue) / (select Sum(Total_Revenue) from stg_churn) as RevPercentage
+from stg_churn
+group by Customer_Status
+
+
+
+
+select State, count(State) as TotalCount,
+count(State) * 100.0/ (select count(*) from stg_churn) as Percentage
+from stg_churn
+group by State
+order by Percentage desc
+
+
+
+select distinct Internet_Type
+from stg_churn
